6702ad19889dd0d6a080728eb225d70844eec380
Reyni <reyni.1210@gmail.com>
2021-06-18 21:22:02 -0400

    ✨ Creates Mapper for carousel items

---
+++ app/src/main/java/com/example/carrousel/network/mapper/CarouselMapper.java
@@ -0,0 +1,22 @@
+package com.example.carrousel.network.mapper;
+
+import com.example.carrousel.model.Image;
+
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarouselMapper {
+    public static CarouselItem toCarouselItem(Image image) {
+        return new CarouselItem(image.getImage());
+    }
+
+    public static List<CarouselItem> toCarouselItem(List<Image> image) {
+        List<CarouselItem> result = new ArrayList<>();
+        for (Image element : image) {
+            result.add(toCarouselItem(element));
+        }
+        return result;
+    }
+}
