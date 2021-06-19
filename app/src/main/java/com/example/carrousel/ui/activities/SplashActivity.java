c368d64f8801be70d79121448f64252891f79c45
Reyni <reyni.1210@gmail.com>
2021-06-18 21:23:47 -0400

    ✨ Creates Splash activity

---
+++ app/src/main/java/com/example/carrousel/ui/activities/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.carrousel.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.carrousel.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
